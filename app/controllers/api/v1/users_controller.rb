6af38dc9b65a847d4a50f046de25bbcc8912c9c9
Nguyen Van Huan <nguyenvanhuan243@gmail.com>
2018-05-26 16:21:00 +0700

    Update User controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,6 +6,8 @@ class Api::V1::UsersController < ApplicationController
 
   api :GET, '/v1/users/:id', 'Show user with id'
   def show
+    # Why using find_by_id: If not exist record it will return nil.
+    # find(id): If not exist record it will raise an exception.
     render json: User.find_by_id(params[:id])
   end
 end
