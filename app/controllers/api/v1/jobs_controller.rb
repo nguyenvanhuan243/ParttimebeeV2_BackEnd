45797c3fbd37ed969f2a8da6fbf2ba0a57d562e3
Nguyen Van Huan <nguyenvanhuan243@gmail.com>
2018-05-28 23:48:26 +0700

    Update job details

--- app/controllers/api/v1/jobs_controller.rb
+++ app/controllers/api/v1/jobs_controller.rb
@@ -3,4 +3,9 @@ class Api::V1::JobsController < ApplicationController
   def index
     render json: Job.all
   end
+  
+  api :GET, '/v1/jobs/:id', 'Get a specify user'
+  def show
+    render json: Job.find_by(id: params[:id])
+  end
 end
