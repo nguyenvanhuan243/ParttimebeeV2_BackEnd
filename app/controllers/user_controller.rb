7af3f6cf7b9842b6dc3efe126e5c8e44b91b1bd0
Nguyen Van Huan <nguyenvanhuan243@gmail.com>
2018-06-09 23:02:40 +0700

    Update User controlller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,10 +1,11 @@
 class UserController < ApplicationController
 	def confirm_email
+		url = Rails.env == 'production' ? 'https://parttimebee.my/test/' : 'http://localhost:3001/test/'
 		user = User.find_by_confirm_token(params[:id])
 		session[:user_id] = user.id
     user.activated = 1
 		user.email_activate
     ModelMailer.wellcome_register(user).deliver
-		redirect_to 'https://parttimebee.my/test/'
+		redirect_to url
 	end
 end
