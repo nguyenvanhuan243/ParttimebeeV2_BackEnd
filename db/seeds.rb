e4c304f68ab34c03bbf7702b45b75b70cd4d0f5d
Nguyen Van Huan <nguyenvanhuan243@gmail.com>
2018-06-15 00:47:46 +0700

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,40 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# 15.times do |i|
-#   User.create!(user_name: 'Huan Nguyen', full_name: 'Nguyen Van Huan', password: '6263e4d438572ce11a', email: 'nguyenthanhlong243@gmail.com',
-# 	address: 'Binh Duong', phone_number: '01664152723', activated: 0, blocked: 0, verified_phone: 0, day_of_birth: '24/03/1995');
+
+# 10.times do |i|
+# 	Job.create!(
+# 		user_id: 2,
+# 		title: Faker::Address.city,
+# 		job_type: 'going',
+# 	);
 # end
 
-10.times do |i|
-	Job.create!(
-		user_id: 2,
-		title: Faker::Address.city,
-		company_name: Faker::Address.community,
-		job_type: 'going',
-	);
-end
+# 10.times do |i|
+# 	Job.create!(
+# 		user_id: 2,
+# 		title: Faker::Address.city,
+# 		job_type: 'pending',
+# 	);
+# end
 
-10.times do |i|
-	Job.create!(
-		user_id: 2,
-		title: Faker::Address.city,
-		company_name: Faker::Address.community,
-		job_type: 'pending',
-	);
-end
+# 10.times do |i|
+# 	Job.create!(
+# 		user_id: 2,
+# 		title: Faker::Address.city,
+# 		job_type: 'expired',
+# 	);
+# end
 
-10.times do |i|
-	Job.create!(
-		user_id: 2,
-		title: Faker::Address.city,
-		company_name: Faker::Address.community,
-		job_type: 'expired',
-	);
-end
-
-# Admin.create!(
-# 	email: 'parttimebee.my@gmail.com',
-# 	password: Digest::MD5.hexdigest('parttimebee.my'),
-# 	token_admin: SecureRandom.urlsafe_base64
-# );
+Admin.create!(
+	email: 'parttimebee.my@gmail.com',
+	password: Digest::MD5.hexdigest('parttimebee.my'),
+	token_admin: SecureRandom.urlsafe_base64
+);
