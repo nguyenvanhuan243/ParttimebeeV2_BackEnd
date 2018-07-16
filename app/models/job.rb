8e03f76624608eff18b4c3d5a7ba3993f6b9ea29
Nguyen Van Huan <nguyenvanhuan243@gmail.com>
2018-07-16 14:44:18 +0700

    Update import job

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,12 @@
 class Job < ApplicationRecord
-  belongs_to :user
+  belongs_to :users
+
+  # Import data to database from csv.
+	def self.import(file)
+      CSV.foreach(file.path, headers: true) do |row|
+        if Job.where(id: row[0]).count.zero?
+          Job.create! row.to_hash
+        end
+      end 
+  end
 end
