ece1b7582812f96f775c04764ee80c194603ae7b
Nguyen Van Huan <nguyenvanhuan243@gmail.com>
2018-07-16 14:05:51 +0700

    Update Disposable Email

--- app/controllers/api/v1/disposable_email_controller.rb
+++ app/controllers/api/v1/disposable_email_controller.rb
@@ -7,11 +7,17 @@ class Api::V1::DisposableEmailController < ApplicationController
 
   api :POST, '/v1/disposable-email/check', 'Check email is included in disposable email list'
   def check_disposable_email
-    email = email_params
+    index = disposable_email_params[:email].index('@') + 1
+    email = disposable_email_params[:email][index..-1]
+    if Disposableemail.find_by(email: email).present?
+      render json: { status: 200, success: true }
+    else
+      render json: { status: 404, success: false }
+    end
   end
 
   private
-  def email_params
+  def disposable_email_params
     params.permit(:email)
   end
 end
