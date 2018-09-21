44ec8add18e79a8570b4fe12d71b189858414e56
Nguyen Van Huan <nguyenvanhuan243@gmail.com>
2018-09-21 22:25:27 +0700

    Update email confirmation

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,7 +1,9 @@
 class UserController < ApplicationController
 	def confirm_email
-		url = Rails.env == 'production' ? 'https://parttimebee.my' : 'http://localhost:3001'
 		user = User.find_by_confirm_token(params[:id])
+		parttimebee = "https://parttimebee.my?confirmed=vwMmq0pDCS3JtyGa-QIwkw&userid=#{user.id}&active=j444fdsafd984utr845r4"
+		localhost = "http://localhost:3001?confirmed=vwMmq0pDCS3JtyGa-QIwkw&userid=#{user.id}&active=j444fdsafd984utr845r4"
+		url = Rails.env == 'production' ? parttimebee : localhost 
 		session[:user_id] = user.id
     user.activated = 1
 		user.email_activate
